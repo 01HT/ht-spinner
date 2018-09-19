86b45fd6a57a9cd7409218784f462242d79acb6e
maksimyurkov <yurkov.maksim.sergeevich@gmail.com>
2018-09-19 12:35:14 +0300

    changes in syntax

--- ht-spinner.js
+++ ht-spinner.js
@@ -1,9 +1,10 @@
-'use strict';
-import { LitElement, html } from '@polymer/lit-element';
+"use strict";
+import { LitElement, html } from "@polymer/lit-element";
 import "@polymer/paper-spinner/paper-spinner.js";
 
 class HTSpinner extends LitElement {
-  _render({}) {
+  render() {
+    const { text } = this;
     return html`
     <style>
     :host {
@@ -13,7 +14,7 @@ class HTSpinner extends LitElement {
     }
 
     paper-spinner {
-      --paper-spinner-stroke-width: 4px;
+      --paper-spinner-stroke-width: 2px;
       width:64px;
       height:64px;
     }
@@ -28,17 +29,60 @@ class HTSpinner extends LitElement {
     :host([button]) paper-spinner {
       width: 32px;
       height: 32px;
-      --paper-spinner-stroke-width: 2px;
       margin-right: 8px;
+    }
+
+    :host([icon-button]) paper-spinner {
+      width: 32px;
+      height: 32px;
+    }
+
+    :host([text]) paper-spinner {
+      width: 32px;
+      height: 32px;
+      margin-right: 16px;
+    }
+
+    :host([text]) #spinner {
+      background: rgba(0, 0, 0, 0.5);
+      background: #fff;
+      padding:16px;
+      border-radius:3px;
+      box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+      0 1px 5px 0 rgba(0, 0, 0, 0.12),
+      0 3px 1px -2px rgba(0, 0, 0, 0.2);
+    }
+
+    #spinner {
+      display: flex;
+      justify-content:center;
+      align-items:center;
+      flex-wrap: wrap;
+    }
+
+    #text {
+      font-size: 14px;
+      line-height: 24px;
+      font-weight: 400;
+      color:var(--secondary-text-color);
     }
     </style>
     <div id="container">
-      <paper-spinner active></paper-spinner>
+      <div id="spinner">
+        <paper-spinner active></paper-spinner>
+        ${text ? html`<div id="text">${text}</div>` : ""}
+      </div>
     </div>`;
   }
 
   static get is() {
-      return 'ht-spinner';
+    return "ht-spinner";
+  }
+
+  static get properties() {
+    return {
+      text: { type: String }
+    };
   }
 }
 
